ebb00e435878d0e85157bba4a725da1f88009471
tjay <github@breaknoise.com>
2018-10-28 14:31:22 +0100

    package python-evdev

--- package/python-evdev/python-evdev.mk
+++ package/python-evdev/python-evdev.mk
@@ -10,8 +10,24 @@ PYTHON_EVDEV_SITE = https://files.pythonhosted.org/packages/7e/53/374b82dd2ccec2
 PYTHON_EVDEV_SETUP_TYPE = setuptools
 PYTHON_EVDEV_LICENSE = MIT
 PYTHON_EVDEV_LICENSE_FILES = PKG-INFO
-PYTHON_EVDEV_INSTALL_TARGET_OPTS = "--evdev-headers $(BUILD_DIR)/linux-headers-custom/include/linux/input.h:$(BUILD_DIR)/linux-headers-custom/include/linux/input-event-codes.h "
-PYTHON_EVDEV_INSTALL_STAGING_OPTS = "--evdev-headers $(BUILD_DIR)/linux-headers-custom/include/linux/input.h:$(BUILD_DIR)/linux-headers-custom/include/linux/input-event-codes.h "
+PYTHON_EVDEV_BUILD_OPTS += --evdev-header $(STAGING_DIR)/usr/include/linux/input.h:$(STAGING_DIR)/usr/include/linux/input-event-codes.h:$(STAGING_DIR)/usr/include/uinput.h
+
+
+
+define PYTHON_EVDEV_BUILD_CMDS
+        cd $(PYTHON_EVDEV_BUILDDIR); \
+                $(PYTHON_EVDEV_BASE_ENV) $(PYTHON_EVDEV_ENV) \
+                $(PYTHON_EVDEV_PYTHON_INTERPRETER) setup.py build build_ecodes \
+                $(PYTHON_EVDEV_BASE_BUILD_OPTS) $(PYTHON_EVDEV_BUILD_OPTS) build_ext
+endef
+
+define PYTHON_EVDEV_INSTALL_TARGET_CMDS
+        cd $(PYTHON_EVDEV_BUILDDIR); \
+                $(PYTHON_EVDEV_BASE_ENV) $(PYTHON_EVDEV_ENV) \
+                $(PYTHON_EVDEV_PYTHON_INTERPRETER) setup.py build build_ecodes \
+                $(PYTHON_EVDEV_BUILD_OPTS) build_ext install \
+                $(PYTHON_EVDEV_BASE_INSTALL_TARGET_OPTS) \
+                $(PYTHON_EVDEV_INSTALL_TARGET_OPTS)
+endef
 
 $(eval $(python-package))
-$(eval $(host-python-package))
