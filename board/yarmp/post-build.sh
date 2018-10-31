3ba246f169169931817f1aa18f54b7d56c167608
tjay <github@breaknoise.com>
2018-10-31 21:19:55 +0100

    aufgeräumt

--- board/yarmp/post-build.sh
+++ board/yarmp/post-build.sh
@@ -11,6 +11,6 @@ set -e
 #fi
 
 if [ -e ${TARGET_DIR}/etc/wpa_supplicant.conf ]; then
-  sed -i 's/{SSID}/${BR2_TARGET_WLAN_SSID}/' ${TARGET_DIR}/etc/wpa_supplicant.conf
-  sed -i 's/{PSK}/${BR2_TARGET_WLAN_PSK}/' ${TARGET_DIR}/etc/wpa_supplicant.conf
+  sed -i 's/{SSID}/'$(grep -oP '(?<=^BR2_TARGET_WLAN_SSID=").+(?="$)' ${BR2_CONFIG})'/' ${TARGET_DIR}/etc/wpa_supplicant.conf
+  sed -i 's/{PSK}/'$(grep -oP '(?<=^BR2_TARGET_WLAN_PSK=").+(?="$)' ${BR2_CONFIG})'/' ${TARGET_DIR}/etc/wpa_supplicant.conf
 fi
