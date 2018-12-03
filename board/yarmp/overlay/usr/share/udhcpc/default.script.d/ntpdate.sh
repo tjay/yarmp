003d1367d0ae64559d5ca37d8e4bad8fcf84d3ab
tjay <github@breaknoise.com>
2018-12-03 18:52:06 +0100

    ntpdate from gateway

--- board/yarmp/overlay/usr/share/udhcpc/default.script.d/ntpdate.sh
+++ board/yarmp/overlay/usr/share/udhcpc/default.script.d/ntpdate.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-#get the gateway - assume that we have there a ntp server
-gateway=$(ip r | awk '$1 == "default" {print $3}')
-
 case "$1" in
 	renew|bound)
-		ntpdate -sp 1 $gateway
+		#get the gateway - assume that we have there a ntp server
+		ntpdate -sp 1 $(ip r | awk '$1 == "default" {print $3}')
 	;;
 esac
